14b0554894cb85885806c0bd6cd875e88801ea20
Amey Gurav <ameyg@marcusevansuk.com>
2025-05-06 13:02:28 +0530

    New Changes in Schemas

--- Events/schemas.py
+++ Events/schemas.py
@@ -1,13 +1,22 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class User(BaseModel):
-    username: str
-    password: str
+    username: str = Field(min_length=3)
+    password: str = Field(min_length=3)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "username": "Enter Username",
+                "password": "Enter Password"
+            }
+        }
+
 
 class Login(BaseModel):
-    username: str
-    password: str
+    username: str=Field(min_length=3)
+    password: str=Field(min_length=3)
 
 
 class Token(BaseModel):
